7f62ebcdfb5814f586ebbd3e6a2f6ec06e8ab6cf
Marc Gonzalez <marcgonzaleztarrio@gmail.com>
2022-10-05 11:13:10 +0200

    fiufiu

---
+++ src/components/Tweet.vue
@@ -0,0 +1,45 @@
+<script setup>
+import User from './User.vue';
+import ProfileImage from './ProfileImage.vue';
+import TimeStamp from './TimeStamp.vue';
+import Message from './Message.vue';
+import Actions from './Actions.vue';
+  defineProps({
+    tweet:{
+      type: Object
+    }
+  })
+
+</script>
+
+<template>
+  <div className="tweet">
+    <ProfileImage
+      :tweets="tweet"
+    />
+    <div className="body">
+      <div className="top">
+        <User
+          :tweet="tweet"
+        />
+        <TimeStamp
+          :tweets="tweet"
+        />
+      </div>
+
+    <Message
+      :tweets="tweet"
+    />
+
+    <Actions/>
+    </div>
+
+    <i class="fas fa-ellipsis-h"></i>
+  </div>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
